d921b2878500850070255f20afef0112dada1e4e
JOSUE DAVID PAZ GOMEZ <jpazg10@miumg.edu.gt>
2025-10-30 23:05:08 -0600

    Se agregó el mantenimientoProyecto

---
+++ Codigo/Modulos/MantenimientoProyecto/MantenimientoProyecto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MantenimientoProyecto
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>a
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            string idUsuario = "admin";
+            Application.Run(new Frm_Proyecto(idUsuario));
+        }
+    }
+}
