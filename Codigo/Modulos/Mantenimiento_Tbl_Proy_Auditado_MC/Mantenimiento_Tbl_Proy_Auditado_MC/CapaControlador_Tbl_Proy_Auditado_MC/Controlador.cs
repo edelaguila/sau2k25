342c6e53b3a7764d4d0a451b62f216164f23b063
502-Alexander! <mcrisostomoa@miumg.edu.gt>
2025-11-01 14:40:45 -0600

    Mantenimiento / Tbl_Proyecto_Auditado / Miguel Crisostomo

---
+++ Codigo/Modulos/Mantenimiento_Tbl_Proy_Auditado_MC/Mantenimiento_Tbl_Proy_Auditado_MC/CapaControlador_Tbl_Proy_Auditado_MC/Controlador.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaControlador_Tbl_Proy_Auditado_MC
+{
+    class Controlador
+    {
+    }
+}
